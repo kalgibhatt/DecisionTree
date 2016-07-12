72241842d718a8a1513d81397501798996870bc3
Kalgi Bhatt <kalgibhatt1@gmail.com>
2016-07-12 12:14:29 -0700

    Initial Commit

---
+++ src/main/java/org/intelligentjava/machinelearning/decisiontree/label/StringLabel.java
@@ -0,0 +1,78 @@
+package org.intelligentjava.machinelearning.decisiontree.label;
+
+/**
+ * Simplest possible label. Simply labels data as true or false.
+ * 
+ * @author Ignas
+ *
+ */
+public class StringLabel extends Label {
+    
+    /** Label. */
+    private String label;
+    
+    /**
+     * Constructor.
+     */
+    private StringLabel(String label) {
+        super();
+        this.label = label;
+    }
+    
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String getPrintValue() {
+        return label;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String getName() {
+        return String.valueOf(label);
+    }
+    
+    /**
+     * Static factory method.
+     */
+    public static Label newLabel(String label) {
+        return new StringLabel(label);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public int hashCode() {
+        return label.hashCode();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        StringLabel other = (StringLabel) obj;
+        if (label.equals(other.label))
+            return false;
+        return true;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String toString() {
+        return "StringLabel [label=" + label + "]";
+    }
+
+}
